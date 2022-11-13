ec3aa06964fe529b2b1cc48a20cf6bf9a701d366
galash <galash@bitbucket.org>
2022-11-13 22:27:54 +0200

    Base program

---
+++ gal_roy.py
@@ -0,0 +1,33 @@
+# Afeka college 2022
+# Assignment 1
+##
+# Create at 13/11/2022
+# Authors:  Gal Ashkenazi
+#           Roy Benjamin
+##
+
+# Question 1 Starter
+def q1():
+    pass
+
+# Question 2 Starter
+def q2():
+    pass
+
+# Question 3 Starter
+def q3():
+    pass
+
+
+
+
+# Main call Questions method
+if __name__ == "__main__":
+    print("---- Question 1 ----")
+    print("Question 1 in comments")
+
+    print("\n---- Question 2 ----\n")
+    q2() # strat question 2
+
+    print("\n---- Question 3 ----\n")
+    q3() # strat question 3
