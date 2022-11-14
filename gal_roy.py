588f6548da7ed51d9a9090e4a1478e934f1214fe
galash <galash@bitbucket.org>
2022-11-14 23:51:58 +0200

    *Change Starter method to Class; *Add needed functions; *DFS method to Q1; *ReadMovies from file method to Q2

--- gal_roy.py
+++ gal_roy.py
@@ -5,21 +5,93 @@
 # Authors:  Gal Ashkenazi
 #           Roy Benjamin
 ##
+import re
 
 # Question 1 Starter
 def q1():
     pass
 
-# Question 2 Starter
-def q2():
-    pass
+# Question 2
+class Question2:
+    # Question 2 constartor
+    def __init__(self, type):
+        # type validation
 
-# Question 3 Starter
-def q3():
-    pass
+        if 'DFS' != type and 'BFS' != type:
+            print("TYPE INVALIDE")
+            return
+        else:
+            self.type = type
 
+        # graph dictionary init 
+        my_graph = {'a': ['b', 'c'], 'b': ['d'], 'c': [], 'd': ['a'], 'e': ['d']}
 
+        # print the reachable list result
+        print(
+            self.reachable(my_graph, 'a')
+        )
+    
+    def reachable(self, graph, node):
+        visit = set() # set init
 
+        # decision reachable method
+        if     self.type == 'DFS': self.DFS(graph, node, visit)
+        elif   self.type == 'BFS': self.BFS(graph, node, visit)
+
+        return list(visit)  # convert and return set result to list
+
+    # DFS method
+    def DFS(self, graph, node, visit):
+        visit.add(node)
+
+        for child in graph[node]:
+            if child not in visit:
+                self.DFS(graph, child, visit)
+
+    # BFS method
+    def BFS(self, graph, node, visit):
+        pass
+        
+        
+# Question 3
+class Question3:
+    # Question 3 constartor
+    def __init__(self):
+        self.movies = dict()    # global movives dict
+
+        self.readMovies("movies.txt")
+
+    # read movives file method
+    def readMovies(self, file_name):
+        for line in open(file= file_name):  # run over all lines in the files
+            line = line.rsplit()
+
+            moviesSet = set()
+            key_flag = True
+            key = ""
+            value = ""
+            
+            for n in line: # run over all words in the line
+                if value != "": value += " "            # add space from the second word
+                value += re.search(r"[^,]*", n).group() # add words exept ',' char
+
+                # keep the key and movies if ',' char detact
+                if re.search(r"\,", n):
+                    if key_flag:    
+                        key = value
+                        key_flag = False
+                    else:
+                        moviesSet.add(value)    # add movie to the set
+
+                    value = ""                  # reuse the value var
+
+            self.movies[key] = moviesSet        # add the actor and ther movies into the global dict
+    
+    def playedWith(self, actor):
+        pass
+
+    def find(self, pattern):
+        pass
 
 # Main call Questions method
 if __name__ == "__main__":
@@ -27,7 +99,8 @@ if __name__ == "__main__":
     print("Question 1 in comments")
 
     print("\n---- Question 2 ----\n")
-    q2() # strat question 2
+    Question2('DFS') # strat question 2 - DFS
+    Question2('BFS') # strat question 2 - BFS
 
     print("\n---- Question 3 ----\n")
-    q3() # strat question 3
+    Question3() # strat question 3
